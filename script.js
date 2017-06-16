e9e80666c47fa55d8051640e52f33257e8766070
Derek Shoemaker <shoemakerdr@gmail.com>
2017-06-16 07:14:58 -0500

    Finished timer implementation. TODO: Create view layer and add controller hooks for view. Also, add controller logic for incrementing/decrementing model.

--- script.js
+++ script.js
@@ -1,41 +1,16 @@
 
 //document.addEventListener('DOMContentLoaded', function() {
 
-
-    // model = {
-    //     state: 'ready', // ready, break, or session
-    //     sessionLength: 25, // 25 minute default length
-    //     breakLength: 5, // 5 minute default length
-    //     currentTimerSeconds: 1500, // current timer state in seconds
-    //     timerString: '25:00', // 25:00 is default timer
-    //     progress: 0, // progress is 1 - 100
-    //     pause: false, // pause starts at false
-    //     // method converts seconds to time formatted string
-    //     secondsToTimeString: sec => {
-    //         return '00:00';
-    //     },
-    //     reset: () => {
-    //         model.state = 'ready';
-    //         model.progress = 0;
-    //         model.pause = false;
-    //         model.updateTimer(model.sessionLength);
-    //     },
-    //     updateTimer: min => {
-    //         model.currentTimerSeconds = (min) ? (min * 60) : (model.currentTimerSeconds - 1);
-    //         model.timerString = model.secondsToTimeString(model.currentTimerSeconds);
-    //     }
-    // };
-
     function TimerModel() {
         this.session = 25;
         this.break = 5;
     }
 
-    TimerModel.prototype.increment = timer => {
+    TimerModel.prototype.increment = function(timer) {
         if (!!this[timer]) this[timer]++;
     };
 
-    TimerModel.prototype.decrement = timer {
+    TimerModel.prototype.decrement = function(timer) {
         if (!!this[timer]) this[timer]--;
     };
 
@@ -43,37 +18,69 @@
         this.model = model;
 
         this.currentTimer = 'session';
-        this.startSeconds = this.model[this.currentTimer] * 60;
-
+        this.seconds = this.model[this.currentTimer] * 60;
+        this.timerID = null;
     }
 
     //methods for controller
 
-    TimerController.prototype.updateModel = () => {
+    TimerController.prototype.updateModel = function() {
 
         console.log(this.model);
     };
 
-    TimerController.prototype.start = countdown => {
-        setInterval(countdown, 1000);
+    TimerController.prototype.start = function() {
+        // render instead of console logs
+        console.log(`Starting ${this.currentTimer}`)
+        console.log(this.secondsToTimeString(this.seconds));
+        let countdown = this.countdown.bind(this);
+        let timerID = setInterval(countdown, 1000);
+        this.timerID = timerID;
     };
 
-    TimerController.prototype.countdown = () => {
-        if (!!this.startSeconds) {
-            this.startSeconds--;
-            console.log(this.startSeconds);
+    TimerController.prototype.secondsToTimeString = function(sec) {
+        const secString = String(sec % 60).length === 1 ?
+                          `0${String(sec % 60)}` :
+                          String(sec % 60);
+        const minString = String(Math.floor(sec / 60));
+        return `${minString}:${secString}`;
+    };
+
+    TimerController.prototype.countdown = function() {
+        if (!!this.seconds) {
+            this.seconds--;
+            // render new count
+            console.log(this.secondsToTimeString(this.seconds));
         }
+        else this.nextTimer();
     };
 
-    TimerController.prototype.nextTimer = () => {
-
+    TimerController.prototype.nextTimer = function() {
+        this.pause();
+        this.currentTimer = this.currentTimer === 'session' ? 'break' : 'session';
+        this.setSeconds(this.currentTimer);
+        this.start();
     };
 
-    TimerController.prototype.pause = timer {
-        clearInterval(timer);
+    TimerController.prototype.setSeconds = function(timer) {
+        this.seconds = this.model[timer] * 60;
     };
 
-    pause
+    TimerController.prototype.pause = function() {
+        clearInterval(this.timerID);
+        // render pause message
+        this.timerID = null;
+    };
+
+    TimerController.prototype.reset = function () {
+        if (this.timerID)
+            this.pause();
+        this.currentTimer = 'session';
+        // render new session timer
+        this.setSeconds('session');
+    };
+
+
 
 
 
